b2f676dea7c15fd392d089d417707d56f634cd72
Joshua <9244335+BuckarewBanzai@users.noreply.github.com>
2021-02-03 00:10:59 -0500

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 DATE=$(date +"%m-%d-%y")
+TSTART=$(date +%s)
+DAYS=7
+
+echo "$(date) - Backup started..." >>backup.log
 
 zip -r "$DATE-world.zip" ~/containers/minecraft/world ~/containers/minecraft/world_nether ~/containers/minecraft/world_the_end
 
-find ~/data/backup/minecraft/ -name "*.zip" -type f -mtime +7 -exec rm -f {} \;
+TEND=$(date +%s)
+
+EXECT=$(($TEND - $TSTART))
+
+echo "$(date) - Backup completed in $EXECT seconds." >>backup.log
+
+echo "$(date) - Deleting files older than $DAYS days." >>backup.log
+
+if  [[ $(find ~/data/backup/minecraft/ -name "*.zip" -type f -mtime +$DAYS) ]]; then
+    echo "$(date) - Old backups found, removing..." >>backup.log
+    find ~/data/backup/minecraft/ -name "*.zip" -type f -mtime +$DAYS -exec rm -f {} \;
+else
+    echo "$(date) - No old backups found." >>backup.log
+fi
